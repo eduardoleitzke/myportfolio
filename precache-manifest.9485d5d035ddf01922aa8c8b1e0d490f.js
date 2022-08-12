1b423f3f32791e3ec4c0fc15039d9c3cd04e3423
Eduardo Leitzke Pinto <95439543+eduardoleitzke@users.noreply.github.com>
2022-08-12 17:30:45 -0300

    Updates

---
+++ precache-manifest.9485d5d035ddf01922aa8c8b1e0d490f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "7694e202f46edaa8e425",
+    "url": "/myportfolio/static/css/main.b152f072.chunk.css"
+  },
+  {
+    "revision": "7694e202f46edaa8e425",
+    "url": "/myportfolio/static/js/main.7694e202.chunk.js"
+  },
+  {
+    "revision": "836e4380fe3f7816a983",
+    "url": "/myportfolio/static/js/1.836e4380.chunk.js"
+  },
+  {
+    "revision": "f1d33015c7dabf2a0de3",
+    "url": "/myportfolio/static/js/2.f1d33015.chunk.js"
+  },
+  {
+    "revision": "9884da9b444d7b7ed268",
+    "url": "/myportfolio/static/js/runtime~main.9884da9b.js"
+  },
+  {
+    "revision": "6ed98dc9b6c0acb38b81d8f382e9bba5",
+    "url": "/myportfolio/static/media/certificado1.6ed98dc9.png"
+  },
+  {
+    "revision": "06cfc2b9c3d94c82792ab985b861733e",
+    "url": "/myportfolio/static/media/certificado2.06cfc2b9.png"
+  },
+  {
+    "revision": "8800d4a5cd7bedccab5350b3ef398fa9",
+    "url": "/myportfolio/static/media/certificado3.8800d4a5.png"
+  },
+  {
+    "revision": "37c37651ceceeb55ee62d3424c0d2db9",
+    "url": "/myportfolio/static/media/certificado4.37c37651.png"
+  },
+  {
+    "revision": "213f301b972331e0519f3222b77558ec",
+    "url": "/myportfolio/static/media/html-5.213f301b.png"
+  },
+  {
+    "revision": "4062c478fe5e8affa8baf0f62ea09851",
+    "url": "/myportfolio/static/media/css-3.4062c478.png"
+  },
+  {
+    "revision": "d52cbbf38c85e9381586462a11027f7c",
+    "url": "/myportfolio/static/media/script-java.d52cbbf3.png"
+  },
+  {
+    "revision": "2fed380a80c1dac5ac33d492159a8c67",
+    "url": "/myportfolio/static/media/atom.2fed380a.png"
+  },
+  {
+    "revision": "7078a27b518a5efa61303b09b04bb1d5",
+    "url": "/myportfolio/static/media/node.7078a27b.png"
+  },
+  {
+    "revision": "3b9ed9c50cab1bbf9ce0cf54401fa81f",
+    "url": "/myportfolio/static/media/momgo.3b9ed9c5.png"
+  },
+  {
+    "revision": "b7fe969fb7d272cfc23fa99a18e8528c",
+    "url": "/myportfolio/static/media/face.b7fe969f.svg"
+  },
+  {
+    "revision": "95b282a4c81340318c8669b24c940543",
+    "url": "/myportfolio/static/media/insta.95b282a4.svg"
+  },
+  {
+    "revision": "493309a8958cfef559bd06d4d5f66279",
+    "url": "/myportfolio/static/media/linkedin.493309a8.svg"
+  },
+  {
+    "revision": "72e9d9ff0d62d34ffd72fa9c8c29a069",
+    "url": "/myportfolio/static/media/whatsapp.72e9d9ff.svg"
+  },
+  {
+    "revision": "0e79ab0e5efaf17ac9ba8e148f83bfa3",
+    "url": "/myportfolio/static/media/email.0e79ab0e.svg"
+  },
+  {
+    "revision": "23752fc640975f5ad65e81ff212f5461",
+    "url": "/myportfolio/static/media/fundo.23752fc6.png"
+  },
+  {
+    "revision": "cdfe47b31e9184a55cf02eef1baf7240",
+    "url": "/myportfolio/static/media/Azonix.cdfe47b3.otf"
+  },
+  {
+    "revision": "97456e3ebaaeaa4be3247dc7677700f2",
+    "url": "/myportfolio/static/media/Leoscar Sans Serif.97456e3e.ttf"
+  },
+  {
+    "revision": "9bbaa8be539d9ccab9874c329d0eb9c6",
+    "url": "/myportfolio/static/media/aqua.9bbaa8be.ttf"
+  },
+  {
+    "revision": "2a03f068216dde85cb408ecff1f8bfda",
+    "url": "/myportfolio/static/media/Eirian.2a03f068.ttf"
+  },
+  {
+    "revision": "b8cdafeb63e201944f5d0ccd9255c312",
+    "url": "/myportfolio/static/media/desenho.b8cdafeb.svg"
+  },
+  {
+    "revision": "40f2d5edb0668f96480f1a93abef0455",
+    "url": "/myportfolio/static/media/moldura.40f2d5ed.svg"
+  },
+  {
+    "revision": "e09e161b287c7bd3784561de1f0629e2",
+    "url": "/myportfolio/index.html"
+  }
+];
